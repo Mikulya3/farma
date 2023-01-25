7275d9d30da5e076c22be10ef2193e8e47ef9d4f
mikulya3 <kadirbekova43@gmail.com>
2023-01-25 17:52:52 +0600

    12

--- main/settings.py
+++ main/settings.py
@@ -184,7 +184,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
